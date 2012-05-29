619238139e9f8d76de003edea51451c38b4403a0
Amit Aryeh Levy <amit@amitlevy.com>
2012-05-29 02:55:35 -0700

    Expresso test framework

--- test/end2end.js
+++ test/end2end.js
@@ -1,23 +1,29 @@
 var MemJS = require('../client');
-var JSTest = require('../lib/test');
 
-JSTest.AddTestSuite({
-  testSet: function() {
-    var client = new MemJS.Client([new MemJS.Server('localhost', 11211)]);
-    client.set('hello', 'world', function(_d) {
-      client.get('hello', function(d) {
-        JSTest.assertEqual('world', d ? d.toString() : d);
-        client.close();
-      });
-    });
-  },
-  testStats: function() {
-    var client = new MemJS.Client([new MemJS.Server('localhost', 11211)]);
-    client.stats(function(d) {
-      JSTest.assert(d['curr_items']);
+exports.testSet = function(beforeExit, assert) {
+  var n = 0;
+  var client = new MemJS.Client([new MemJS.Server('localhost', 11211)]);
+  client.set('hello', 'world', function(_d) {
+    client.get('hello', function(d) {
+      assert.equal('world', d ? d.toString() : d);
       client.close();
+      n += 1;
     });
-  }
-});
+  });
+  beforeExit(function() {
+    assert.equal(1, n,  'Ensure set and get are called')
+  });
+}
 
-JSTest.RunTests();
+exports.testStats = function(beforeExit, assert) {
+  var n = 0;
+  var client = new MemJS.Client([new MemJS.Server('localhost', 11211)]);
+  client.stats(function(d) {
+    assert.isDefined(d['curr_items']);
+    client.close();
+    n += 1;
+  });
+  beforeExit(function() {
+    assert.equal(1, n,  'Ensure stats is called')
+  });
+}
--- test/header_test.js
+++ test/header_test.js
@@ -1,35 +1,31 @@
 var header = require('../header');
-var JSTest = require('../lib/test');
 
-JSTest.AddTestSuite({
-  testParseHeaderResponse: function() {
-    var headerBuf = new Buffer([0x81, 1, 7, 0, 4, 0, 0, 1, 0, 0, 0, 9, 0, 0, 0, 0, 0x0a, 0, 0, 0, 0, 0, 0, 0]);
-    var responseHeader = header.fromBuffer(headerBuf);
-    JSTest.assertEqual(0x81, responseHeader.magic);
-    JSTest.assertEqual(1, responseHeader.opcode);
-    JSTest.assertEqual(0x0700, responseHeader.keyLength);
-    JSTest.assertEqual(4, responseHeader.extrasLength);
-    JSTest.assertEqual(0, responseHeader.dataType);
-    JSTest.assertEqual(1, responseHeader.status);
-    JSTest.assertEqual(9, responseHeader.totalBodyLength);
-    JSTest.assertEqual(0, responseHeader.opaque);
-    JSTest.assertEqual(new Buffer([0x0a, 0, 0, 0, 0, 0, 0, 0]).toString(), responseHeader.cas);
-  },
-  testDumpHeader: function() {
-    responseHeader = {
-      magic: 0x81,
-      opcode: 1,
-      keyLength: 0x700,
-      extrasLength: 4,
-      dataType: 0,
-      status: 1,
-      totalBodyLength: 9,
-      opaque: 0,
-      cas: new Buffer([0x0a, 0, 0, 0, 0, 0, 0, 0])
-    }
-    expected = new Buffer([0x81, 1, 7, 0, 4, 0, 0, 1, 0, 0, 0, 9, 0, 0, 0, 0, 0x0a, 0, 0, 0, 0, 0, 0, 0]);
-    JSTest.assertEqual(header.toBuffer(responseHeader).toString(), expected.toString());
+exports.testParseHeaderResponse = function(be, assert) {
+  var headerBuf = new Buffer([0x81, 1, 7, 0, 4, 0, 0, 1, 0, 0, 0, 9, 0, 0, 0, 0, 0x0a, 0, 0, 0, 0, 0, 0, 0]);
+  var responseHeader = header.fromBuffer(headerBuf);
+  assert.equal(0x81, responseHeader.magic);
+  assert.equal(1, responseHeader.opcode);
+  assert.equal(0x0700, responseHeader.keyLength);
+  assert.equal(4, responseHeader.extrasLength);
+  assert.equal(0, responseHeader.dataType);
+  assert.equal(1, responseHeader.status);
+  assert.equal(9, responseHeader.totalBodyLength);
+  assert.equal(0, responseHeader.opaque);
+  assert.equal(new Buffer([0x0a, 0, 0, 0, 0, 0, 0, 0]).toString(), responseHeader.cas);
+}
+
+var testDumpHeader= function(be, assert) {
+  responseHeader = {
+    magic: 0x81,
+    opcode: 1,
+    keyLength: 0x700,
+    extrasLength: 4,
+    dataType: 0,
+    status: 1,
+    totalBodyLength: 9,
+    opaque: 0,
+    cas: new Buffer([0x0a, 0, 0, 0, 0, 0, 0, 0])
   }
-});
-
-JSTest.RunTests();
+  expected = new Buffer([0x81, 1, 7, 0, 4, 0, 0, 1, 0, 0, 0, 9, 0, 0, 0, 0, 0x0a, 0, 0, 0, 0, 0, 0, 0]);
+  assert.equal(header.toBuffer(responseHeader).toString(), expected.toString());
+}
