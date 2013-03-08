492bbabffe3aa702502baa5a300e35e49a0115c3
Amit Aryeh Levy <amit@amitlevy.com>
2013-03-08 10:35:22 -0800

    Better benchmarking

--- bench/memjs.js
+++ bench/memjs.js
@@ -1,20 +1,96 @@
-var MemJS = require('memjs').Client;
+var memjs = require("memjs")
+var header = require('header');
+var b = require("benchmark")
 
-var client = MemJS.create();
+var x = (function() {
+  suite = new b.Suite;
 
-for (var i = 0; i < 1; i += 1) {
-  (function() {
-    var j = 2500 * 6;
-    var prev = Date.now();
-    var listener = function() {
-      if (j > 0) {
-        j -= 1;
-        client.get("hello", listener);
-      } else {
-        console.log(Date.now() - prev);
-      }
-    };
-    listener();
-  })();
+  var headerBuf = new Buffer([0x81, 1, 7, 0, 4, 0, 0, 1, 0, 0, 0, 9, 0, 0, 0, 0, 0x0a, 0, 0, 0, 0, 0, 0, 0]);
+  var parsedHeader = header.fromBuffer(headerBuf);
+
+  suite.add('Header#fromBuffer', function() {
+    header.fromBuffer(headerBuf);
+  })
+  .add('Header#toBuffer', function() {
+    header.toBuffer(parsedHeader);
+  })
+  // add listeners
+  .on('cycle', function(event) {
+    console.log(String(event.target));
+  })
+  // run async
+  .run({ 'async': true });
+})();
+
+x = (function() {
+  suite = new b.Suite;
+  var responseHeader = {
+    magic: 0x81,
+    opcode: 1,
+    keyLength: 15,
+    extrasLength: 0,
+    dataType: 0,
+    status: 1,
+    totalBodyLength: 1024 * 10 + 15,
+    opaque: 0,
+    cas: new Buffer([0x0a, 0, 0, 0, 0, 0, 0, 0])
+  }
+  var buf = new Buffer(24 + 15 + 1024 * 10);
+  header.toBuffer(responseHeader).copy(buf);
+  buf.write(makeString(55));
+
+  var server = new memjs.Server();
+
+  var dummyFunc = function(arg) {
+    server.onResponse(dummyFunc);
+    return arg;
+  };
+
+  for (var i = 0; i < 10; i++) {
+    server.onResponse(dummyFunc);
+  }
+
+  suite.add('Server#respond', function() {
+    server.respond('helloworldthisisatesttohowmuchyouareaversetocopyingoverthestargatecommisionandromanticizingaboutmylifelongpassionforlearningandyieldingtoyourdesires');
+  })
+  .add('Server#responseHandler', function() {
+    server.responseHandler(buf);
+  })
+  // add listeners
+  .on('cycle', function(event) {
+    console.log(String(event.target));
+  })
+  // run async
+  .run({ 'async': true });
+})();
+
+function makeString(n) {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for( var i=0; i < n; i++ )
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
 }
+
+x = (function() {
+  suite = new b.Suite;
+  client = memjs.Client.create();
+
+  suite.cycles = 0;
+  suite.add('Client#get', function() {
+    client.get("hello", function(err, val) {
+      suite.cycles++;
+    });
+  })
+  // add listeners
+  .on('cycle', function(event) {
+    console.log(String(event.target) + "     " + suite.cycles);
+  });
+  client.set("hello", makeString(10240), function(err, val) {
+    // run async
+    suite.run({ 'async': true });
+  });
+})();
 
