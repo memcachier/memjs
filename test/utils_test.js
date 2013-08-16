c612bdc23da3562d70337246bdf82ec94011c3cd
Amit Aryeh Levy <amit@amitlevy.com>
2013-08-15 19:37:58 -0700

    Test when first parameter is null

--- test/utils_test.js
+++ test/utils_test.js
@@ -1,12 +1,21 @@
 utils = require('../lib/memjs/utils')
 
 exports.testMergePresereParameter = function(beforeExit, assert) {
-  result = utils.merge({}, { retries: 2 });
+  var result = utils.merge({}, { retries: 2 });
   assert.equal(2, result.retries);
 }
 
 exports.testMergePresereParameterWhenZero = function(beforeExit, assert) {
-  result = utils.merge({ retries: 0 }, { retries: 2 });
+  var result = utils.merge({ retries: 0 }, { retries: 2 });
   assert.equal(0, result.retries);
 }
+
+exports.testMergeDontPresereParameterWhenUndefinedOrNull =
+  function(beforeExit, assert) {
+    var result = utils.merge({ retries: undefined }, { retries: 2 });
+    assert.equal(2, result.retries);
+
+    var result2 = utils.merge({ retries: null }, { retries: 2 });
+    assert.equal(2, result2.retries);
+  }
 
