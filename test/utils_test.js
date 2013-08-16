c23ffb70a9454f1e5c2b7acb0fce3034b39abaa9
Amit Aryeh Levy <amit@amitlevy.com>
2013-08-15 19:35:32 -0700

    Modify tests to use testing framework

--- test/utils_test.js
+++ test/utils_test.js
@@ -1,22 +1,12 @@
 utils = require('../lib/memjs/utils')
 
-console.log('merge should preserve value passed in first parameter');
-
-
-result = utils.merge({}, { retries: 2 });
-
-if (result.retries == 2) {
-  console.log('Looks good.');
-} else {
-  console.log('It doesn\'t work. Expected { retries: 2 } , Got:', result);
+exports.testMergePresereParameter = function(beforeExit, assert) {
+  result = utils.merge({}, { retries: 2 });
+  assert.equal(2, result.retries);
 }
 
-result = utils.merge({ retries: 0 }, { retries: 2 });
-
-if (result.retries == 0) {
-  console.log('Looks good.');
-} else {
-  console.log('It doesn\'t work. Expected { retries: 0 } , Got:', result);
+exports.testMergePresereParameterWhenZero = function(beforeExit, assert) {
+  result = utils.merge({ retries: 0 }, { retries: 2 });
+  assert.equal(0, result.retries);
 }
-
 
