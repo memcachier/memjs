7dabcd38b568e1d2117137b576d39e76d4b743bb
Jeff Miccolis <jeff@miccolis.net>
2015-12-23 00:16:15 -0500

    Don't return a string from makeAmountInitialAndExpiration

--- test/utils_test.js
+++ test/utils_test.js
@@ -19,3 +19,27 @@ exports.testMergeDontPresereParameterWhenUndefinedOrNull =
     assert.equal(2, result2.retries);
   }
 
+exports.testMakeAmountInitialAndExpiration = function(beforeExit, assert) {
+    var extras, buf, fixture;
+    extras = utils.makeAmountInitialAndExpiration(1, 1, 1);
+    fixture = new Buffer('0000000000000001000000000000000100000001', 'hex');
+    assert.equal(20, extras.length);
+    assert.equal(fixture.toString('hex'), extras.toString('hex'));
+    buf = new Buffer(extras);
+    assert.equal(20, buf.length);
+    assert.equal(fixture.toString('hex'), buf.toString('hex'));
+
+    extras = utils.makeAmountInitialAndExpiration(255, 1, 1);
+    fixture = new Buffer('00000000000000ff000000000000000100000001', 'hex');
+    assert.equal(20, extras.length);
+    assert.equal(fixture.toString('hex'), extras.toString('hex'));
+    buf = new Buffer(extras);
+    assert.equal(20, buf.length);
+    assert.equal(fixture.toString('hex'), buf.toString('hex'));
+}
+
+exports.testMakeRequestBufferExtrasLength = function(beforeExit, assert) {
+    extras = utils.makeAmountInitialAndExpiration(255, 1, 1);
+    var buf = utils.makeRequestBuffer(0, 'test', extras, 1, 0);
+    assert.equal(20, buf[4]);
+}
