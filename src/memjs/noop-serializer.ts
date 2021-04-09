efc52d6f0d7450cbcf28a84cb59a0a6d2fe4509e
Jake Teton-Landis <just.1.jake@gmail.com>
2021-04-09 13:49:36 -0700

    serializer.ts

--- src/memjs/noop-serializer.ts
+++ src/memjs/noop-serializer.ts
@@ -1,10 +1,24 @@
-var noopSerializer = {
+import { OP } from "./constants";
+import { MaybeBuffer } from "./utils";
+
+interface Serializer<Value, Extras> {
+  serialize(
+    opcode: OP,
+    value: Value,
+    extras: Extras
+  ): { value: MaybeBuffer; extras: MaybeBuffer };
+  deserialize(
+    opcode: OP,
+    value: MaybeBuffer,
+    extras: MaybeBuffer
+  ): { value: Value; extras: Extras };
+}
+
+export const noopSerializer: Serializer<MaybeBuffer, MaybeBuffer> = {
   serialize: function (opcode, value, extras) {
     return { value: value, extras: extras };
   },
   deserialize: function (opcode, value, extras) {
     return { value: value, extras: extras };
-  }
+  },
 };
-
-exports.noopSerializer = noopSerializer;
