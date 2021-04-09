998ae68b7f8d28a2168ad384254d310b5786eb92
Jake Teton-Landis <just.1.jake@gmail.com>
2021-04-09 13:27:29 -0700

    protocol.ts

--- src/memjs/protocol.ts
+++ src/memjs/protocol.ts
@@ -1,19 +1,18 @@
-// # MemJS Memcache binary protocol errors
-
-exports.errors = {};
-exports.errors[0x0000] = 'No error';
-exports.errors[0x0001] = 'Key not found';
-exports.errors[0x0002] = 'Key exists';
-exports.errors[0x0003] = 'Value too large';
-exports.errors[0x0004] = 'Invalid arguments';
-exports.errors[0x0005] = 'Item not stored';
-exports.errors[0x0006] = 'Incr/Decr on non-numeric value';
-exports.errors[0x0007] = 'The vbucket belongs to another server';
-exports.errors[0x0008] = 'Authentication error';
-exports.errors[0x0009] = 'Authentication continue';
-exports.errors[0x0081] = 'Unknown command';
-exports.errors[0x0082] = 'Out of memory';
-exports.errors[0x0083] = 'Not supported';
-exports.errors[0x0084] = 'Internal error';
-exports.errors[0x0085] = 'Busy';
-exports.errors[0x0086] = 'Temporary failure';
+/** MemJS Memcache binary protocol errors */
+export const errors: { [key: number]: string } = {};
+errors[0x0000] = "No error";
+errors[0x0001] = "Key not found";
+errors[0x0002] = "Key exists";
+errors[0x0003] = "Value too large";
+errors[0x0004] = "Invalid arguments";
+errors[0x0005] = "Item not stored";
+errors[0x0006] = "Incr/Decr on non-numeric value";
+errors[0x0007] = "The vbucket belongs to another server";
+errors[0x0008] = "Authentication error";
+errors[0x0009] = "Authentication continue";
+errors[0x0081] = "Unknown command";
+errors[0x0082] = "Out of memory";
+errors[0x0083] = "Not supported";
+errors[0x0084] = "Internal error";
+errors[0x0085] = "Busy";
+errors[0x0086] = "Temporary failure";
