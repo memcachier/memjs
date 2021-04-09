fc911c6b1358effbbeee57503dda0d84fa6a9ff1
Jake Teton-Landis <just.1.jake@gmail.com>
2021-04-09 13:24:38 -0700

    tsify header.ts

--- src/memjs/header.ts
+++ src/memjs/header.ts
@@ -1,27 +1,41 @@
 // # MemJS Memcache binary protocol header
 
-// fromBuffer converts a serialized header to a JS object.
-exports.fromBuffer = function(headerBuf) {
+import { OP, ResponseStatus } from "./constants";
+
+export interface Header {
+  magic: number;
+  opcode: OP;
+  keyLength: number;
+  extrasLength: number;
+  dataType: number;
+  status: ResponseStatus;
+  totalBodyLength: number;
+  opaque: number;
+  cas: Buffer;
+}
+
+/** fromBuffer converts a serialized header to a JS object. */
+export function fromBuffer(headerBuf: Buffer): Header | {} {
   if (!headerBuf) {
     return {};
   }
   return {
-    magic:           headerBuf.readUInt8(0),
-    opcode:          headerBuf.readUInt8(1),
-    keyLength:       headerBuf.readUInt16BE(2),
-    extrasLength:    headerBuf.readUInt8(4),
-    dataType:        headerBuf.readUInt8(5),
-    status:          headerBuf.readUInt16BE(6),
+    magic: headerBuf.readUInt8(0),
+    opcode: headerBuf.readUInt8(1),
+    keyLength: headerBuf.readUInt16BE(2),
+    extrasLength: headerBuf.readUInt8(4),
+    dataType: headerBuf.readUInt8(5),
+    status: headerBuf.readUInt16BE(6),
     totalBodyLength: headerBuf.readUInt32BE(8),
-    opaque:          headerBuf.readUInt32BE(12),
-    cas:             headerBuf.slice(16, 24)
+    opaque: headerBuf.readUInt32BE(12),
+    cas: headerBuf.slice(16, 24),
   };
-};
+}
 
-// toBuffer converts a JS memcache header object to a binary memcache header
-exports.toBuffer = function(header) {
+/** toBuffer converts a JS memcache header object to a binary memcache header */
+export function toBuffer(header: Header) {
   var headerBuf = Buffer.alloc(24);
-  headerBuf.fill();
+  headerBuf.fill(0);
   headerBuf.writeUInt8(header.magic, 0);
   headerBuf.writeUInt8(header.opcode, 1);
   headerBuf.writeUInt16BE(header.keyLength, 2);
@@ -33,7 +47,7 @@ exports.toBuffer = function(header) {
   if (header.cas) {
     header.cas.copy(headerBuf, 16);
   } else {
-    headerBuf.fill('\x00', 16);
+    headerBuf.fill("\x00", 16);
   }
   return headerBuf;
-};
+}
