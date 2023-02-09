bbf0e849fdc44f08ba93bf0df4b1e2b5b69f66ba
Arturo Olvera <arturo.olvera@makenotion.com>
2023-02-09 15:10:12 -0800

    comments

--- src/memjs/memjs.ts
+++ src/memjs/memjs.ts
@@ -913,6 +913,9 @@ class Client<Value = MaybeBuffer, Extras = MaybeBuffer> {
    * Retrieves the server version from all the servers
    * in the backend pool, errors if any one of them has an
    * error
+   * 
+   * Callbacks functions are called before/after we ping memcached
+   * and used to log which hosts are timing out.
    */
   async versionAll(callbacks?: {
     beforePing?: (serverKey: string) => void;
