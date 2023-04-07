2a19d80e5160ebc3f6e8ee9bd80199bda0484d12
Leon Wei <leon@makenotion.com>
2023-04-07 13:11:36 -0700

    Hanlde server restart

--- smoketest.js
+++ smoketest.js
@@ -2,9 +2,25 @@
 const MemJS = require(".");
 const assert = require("assert");
 const client = MemJS.Client.create();
+// We cannot fake Memcached server close for more than 1 second.
+// So we reduce the retries to fake the server down.
+const client2 = MemJS.Client.create(undefined, { retries: 1 });
 
 function assertBufferEqual(buf1, buf2, msg) {
   assert.strictEqual(buf1.toString(), buf2.toString(), msg);
+}
+
+async function testCloseConnection() {
+  await client2.set("foo2", "1");
+  let { value } = await client2.get("foo2");
+  assert.strictEqual(value.toString(), "1");
+
+  client2.close();
+  // Some time to handle socket close event.
+  await new Promise((resolve) => setTimeout(resolve, 10));
+
+  const v3 = await client2.get("foo2");
+  assert.strictEqual(v3.value.toString(), value.toString(), "did not get set");
 }
 
 async function body() {
@@ -33,6 +49,8 @@ async function body() {
     cas3.toString(),
     "has new CAS after identical set"
   );
+
+  await testCloseConnection();
 }
 
 async function main() {
@@ -44,6 +62,7 @@ async function main() {
     process.exit(1);
   } finally {
     client.quit();
+    client2.quit();
   }
 }
 
